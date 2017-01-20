f978ac403217fdcb650cda5fe315a9df50dc3630
lh-wheeve <lakshitha.herath@wheeve.co.uk>
2017-01-20 18:36:07 +0530

    new java project

---
+++ lambdaJava/src/main/java/com/poc/UserRequest.java
@@ -0,0 +1,30 @@
+package com.poc;
+
+public class UserRequest {
+
+	private String name;
+	private Integer age;
+	
+	public UserRequest()
+	{
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	
+	
+}
