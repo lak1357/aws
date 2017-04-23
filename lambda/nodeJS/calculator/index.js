f4cd0c0ac3f1866a1008988cc475fc2970107167
Lakshitha Herath <lakshitha@Lakshithas-MacBook-Pro.local>
2017-04-23 21:19:44 +0530

    new modifications

--- lambda/nodeJS/calculator/index.js
+++ lambda/nodeJS/calculator/index.js
@@ -1,3 +1,31 @@
 exports.handler = (event, context, callback) => {
-    callback(null, 'Hello from Mitra');
+
+    var operation = event.operation;
+    var number1 = event.number1;
+    var number2 = event.number2;
+    let response;
+
+    if (operation === undefined) {
+
+        response = {
+            error: 'No operation specified'
+        };
+
+    }
+    else {
+
+        var result;
+
+        if (operation == "sum") {
+            result = number1 + number2;
+        }
+        else if (operation == "subtract"){
+            result = number1 - number2;
+        }
+
+        response = {
+            result: result
+        };
+    }
+    callback(null, response);
 };
