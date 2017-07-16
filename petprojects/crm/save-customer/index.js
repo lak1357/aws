46bdf6a0356502a833ccc5c54173af1400b54873
Lakshitha Herath <lakshitha@Lakshithas-MacBook-Pro.local>
2017-07-16 13:17:00 +0530

    new changes

--- petprojects/crm/save-customer/index.js
+++ petprojects/crm/save-customer/index.js
@@ -7,26 +7,21 @@ exports.handler = (event, context, callback) => {
     var address = event.address;
     var phone = event.phone;
     let response;
+    var message;
 
 
     if (name === undefined || age === undefined || address === undefined || phone === undefined) {
 
-        let response = {
-            statusCode: '400',
-            body: JSON.stringify({ error: 'Bad request' }),
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        };
-
-        context.succeed(response);
+        message = {name:"Error"};
 
     }
     else {
 
-        var message = {name:name , age:age , address:address , phone:phone};
-        var sns = new AWS.SNS();
-        sns.publish({
+        message = {name:name , age:age , address:address , phone:phone};
+    }
+
+            var sns = new AWS.SNS();
+            sns.publish({
             TopicArn: "arn:aws:sns:ap-southeast-1:936825406282:customerDetails",
             Subject: "Test SNS From Lambda",
             Message: JSON.stringify(message)
@@ -40,5 +35,4 @@ exports.handler = (event, context, callback) => {
             }
         });
 
-    }
 };
