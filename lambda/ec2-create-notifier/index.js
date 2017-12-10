5d9b8ca45e0fa18a35260cb4ad066fcb7fb9335c
Lakshitha Herath <lakshitha@Lakshithas-MacBook-Pro.local>
2017-12-10 14:24:25 +0530

    SAM created

--- lambda/ec2-create-notifier/index.js
+++ lambda/ec2-create-notifier/index.js
@@ -1,33 +1,22 @@
+'use strict';
+
 exports.handler = (event, context, callback) => {
-
-    var operation = event.operation;
-    var number1 = event.number1;
-    var number2 = event.number2;
-    let response;
-
-
-    if (operation === undefined) {
-
-        return context.fail('Bad Request: No operation specified');
-
-    }
-    else {
-
-        var result;
-
-        if (operation == "add") {
-            result = number1 + number2;
-        }
-        else if (operation == "subtract") {
-            result = number1 - number2;
-        }
-
-        response = {
-            result: result
-        };
-
-
-        return context.succeed(response);
-
-    }
+    console.log('LogScheduledEvent');
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    callback(null, 'Finished');
 };
+
+
+
+
+-------------
+{
+    "source": [
+      "aws.ec2"
+    ],
+    "detail": {
+        "eventName": [
+          "RunInstances"
+        ]
+      }
+}
