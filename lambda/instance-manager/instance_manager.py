92c24a76e64eb4026af9068910303d5b43d3fec7
lakshithaH <lakshitha.herath@moneta.cz>
2019-04-06 12:43:50 +0530

    sample

--- lambda/instance-manager/instance_manager.py
+++ lambda/instance-manager/instance_manager.py
@@ -6,6 +6,6 @@ print('Loading Function')
 
 def my_handler(event, context):
     ec2 = boto3.resource('ec2')
-    filters = [{'Name':'tag:Environment', 'Values':['Dev']}]
+    filters = [{'Name':'tag:Environment', 'Values':['vpbank-aws-dev-env']}]
     ec2.instances.filter(Filters=filters).stop()
     return 'Dev instances shutted down'
